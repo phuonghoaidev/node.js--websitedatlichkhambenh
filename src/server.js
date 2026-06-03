d7e558f1b67ecdffc337561fe96718e6ba900680
Phan Phuong <phuonghoaik12@gmail.com>
2026-06-03 15:52:24 +0700

    capnhat

--- src/server.js
+++ src/server.js
@@ -8,12 +8,34 @@ import connectDB from "./config/connectDB";
 require("dotenv").config();
 
 let app = express();
+// app.use(cors({
+//     origin: true,
+//     credentials: true
+// }));
 
-// config cors
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
+
 
 // config app
 app.use(bodyParser.json());
