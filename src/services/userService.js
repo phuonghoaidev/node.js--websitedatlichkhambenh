c065d3f726949b1d12ae9841b57510017e530a72
Phan Phuong <phuonghoaik12@gmail.com>
2026-07-08 23:25:24 +0700

    cap nhat

--- src/services/userService.js
+++ src/services/userService.js
@@ -27,7 +27,7 @@ let handlUserLogin = (email, password) => {
                 //user already exist
 
                 let user = await db.User.findOne({
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                     where: { email: email },
                     raw: true
                 })
